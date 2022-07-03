e391c21506813e183684b277b44744a57db56a80
Kovacsics Robert <kovirobi@gmail.com>
2022-07-03 22:48:30 +0100

    Initial commit, from nixpkgs issue 106759
    https://github.com/NixOS/nixpkgs/issues/106759#issuecomment-1133748080
    
    input 'nixpkgs':
     'github:NixOS/nixpkgs/0ea7a8f1b939d74e5df8af9a8f7342097cdf69eb' (2022-07-02)

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+
+    nixosConfigurations.cross-vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, lib, modulesPath, ... }: {
+          nixpkgs = {
+            crossSystem = lib.systems.examples.aarch64-multiplatform;
+          };
+
+          imports = [ (modulesPath + "/installer/sd-card/sd-image-aarch64.nix") ];
+
+          # Let 'nixos-version --json' know about the Git revision
+          # of this flake.
+          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+
+          users.mutableUsers = false;
+          users.users.root = {
+            password = "root";
+          };
+          users.users.user = {
+            password = "user";
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+          };
+          system.stateVersion = "22.05";
+        })
+      ];
+    };
+
+    packages.x86_64-linux.default = self.nixosConfigurations.cross-vm.config.system.build.sdImage;
+  };
+
+}
