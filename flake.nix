852dc73421cff65a27ce0b164bef1beb5979cb45
Kovacsics Robert <kovirobi@gmail.com>
2023-02-05 23:30:13 +0000

    Update config to have passwords, tailscale and SSH

--- flake.nix
+++ flake.nix
@@ -19,14 +19,27 @@
 
           users.mutableUsers = false;
           users.users.root = {
-            password = "root";
+            hashedPassword = "$y$j9T$92t3XJPmBHWk1baoc0WTu/$DAC5AJseext1xVG7N0PK2tHYJ4L0qEZpbJleu1V5sS5";
           };
           users.users.user = {
-            password = "user";
+            hashedPassword = "$y$j9T$92t3XJPmBHWk1baoc0WTu/$DAC5AJseext1xVG7N0PK2tHYJ4L0qEZpbJleu1V5sS5";
             isNormalUser = true;
             extraGroups = [ "wheel" ];
+            openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHfojmpABobr3CEgN3eCu3mEqpr3oAs2h5NfY1ZtppIK ccl\\rmk@RMKW10L2"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxS1hIoi4jj4h00KoIfBJJX6aMF5TtdZZxBOqLRRKCH rmk35 pc id_ed25519"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCjMm+GhqvoboXvOJO/MuLccb606oJyHYyL4Mo3kuLO u0_a301@Legion Duel"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKG9Dg3j/KJgDbtsUSyOJBF7+bQzfDQpLo4gqDX195rJ rmk@rmk-cc-pc-nixos-a"
+            ];
           };
           system.stateVersion = "22.11";
+
+          security.sudo.wheelNeedsPassword = false;
+
+          services.tailscale.enable = true;
+
+          services.openssh.enable = true;
+          services.openssh.settings.PermitRootLogin = "no";
         })
       ];
     };
