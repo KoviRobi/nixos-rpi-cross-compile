8862cf28aac2f96e687f5a24efe5cc96c2208c53
Kovacsics Robert <kovirobi@gmail.com>
2023-02-06 00:10:33 +0000

    Add my overlays

--- flake.nix
+++ flake.nix
@@ -1,14 +1,22 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.kovirobi.url = "github:KoviRobi/nixos-config/flake";
+  inputs.kovirobi.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.kovirobi.inputs.home-manager.follows = "home-manager";
+
+  outputs = { self, nixpkgs, home-manager, kovirobi }: {
 
     nixosConfigurations.cross-vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [
+      modules = let system = "aarch64-linux"; in [
         ({ pkgs, lib, modulesPath, ... }: {
           nixpkgs = {
             crossSystem = lib.systems.examples.aarch64-multiplatform;
+            overlays = builtins.attrValues kovirobi.overlays;
           };
 
           networking.hostName = "pi";
@@ -24,7 +32,7 @@
           users.users.root = {
             hashedPassword = "$y$j9T$92t3XJPmBHWk1baoc0WTu/$DAC5AJseext1xVG7N0PK2tHYJ4L0qEZpbJleu1V5sS5";
           };
-          users.users.user = {
+          users.users.rmk = {
             hashedPassword = "$y$j9T$92t3XJPmBHWk1baoc0WTu/$DAC5AJseext1xVG7N0PK2tHYJ4L0qEZpbJleu1V5sS5";
             isNormalUser = true;
             extraGroups = [ "wheel" ];
@@ -44,6 +52,22 @@
           services.openssh.enable = true;
           services.openssh.settings.PermitRootLogin = "no";
         })
+
+        home-manager.nixosModule
+        {
+          # environment.systemPackages = [ home-manager.defaultPackage.${system} ];
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.rmk = {
+            imports = kovirobi.homeModules.simple;
+            home.stateVersion = "22.11";
+            # Fails to cross-compile
+            # Probably
+            # https://git.sr.ht/~rycee/nmd/tree/409f1310b168f96c6c8b556d24731a3e7c26c255/item/lib/modules-docbook.nix
+            # should have nix-instantiate from nativeBuildInputs
+            manual.manpages.enable = false;
+          };
+        }
       ];
     };
 
